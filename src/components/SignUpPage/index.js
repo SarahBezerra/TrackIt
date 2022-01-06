e754f82316a2752bd2b7d278fa131548aa9cbeb0
Sarah <sarahbezerra2013@gmail.com>
2022-01-06 01:49:52 -0300

    Tela de Cadastro: enviando dados para a API

--- src/components/SignUpPage/index.js
+++ src/components/SignUpPage/index.js
@@ -1,17 +1,43 @@
-import React from "react"
+import axios from "axios"
+import React, { useState } from "react"
 import logo from "../../images/logo.png"
 import { Image, Form, StyledLink } from './style.js'
 
 export default function SignUpPage(){
+
+    const [ registrationData, setRegistrationData ] = useState({
+            email: "",
+            name: "",
+            image: "",
+            password: ""
+        }
+    )
+
+    function handleInputChange(e){
+        setRegistrationData({...registrationData, [e.target.name]: e.target.value});
+        console.log(e.target.name +" : "+ e.target.value)
+        console.log(registrationData)
+    }
+
+    function handleSignUp(e){
+        e.preventDefault();
+
+        const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up",
+        registrationData);
+        
+        promise.then(response => console.log(response));
+        promise.catch(error => console.log(error.response));
+    }
+
     return(
         <>
         <Image src={logo} alt="logo TrackIt" />
-        <Form>
-            <input type="email" placeholder="email" name="email" required />
-            <input type="password" placeholder="senha" name="password" required />
-            <input type="text" placeholder="nome" name="name" required />
-            <input type="url" placeholder="foto" name="photo" required />
-            <button>Cadastrar</button>
+        <Form onSubmit={handleSignUp}>
+            <input type="email" placeholder="email" name="email" value={registrationData.email} onChange={handleInputChange} required />
+            <input type="password" placeholder="senha" name="password" value={registrationData.password} onChange={handleInputChange} required />
+            <input type="text" placeholder="nome" name="name" value={registrationData.name} onChange={handleInputChange} required />
+            <input type="url" placeholder="foto" name="image" value={registrationData.image} onChange={handleInputChange} required />
+            <button type="submit">Cadastrar</button>
         </Form>
         <StyledLink to="/">Já tem uma conta? Faça login!</StyledLink>
         </>
