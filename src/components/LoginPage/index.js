c1fff1b33efb3cabf38ba953b565c4cf36cec162
Sarah <sarahbezerra2013@gmail.com>
2022-01-06 21:57:33 -0300

    Tela de Login: Enviando dados para a API e Layout de tela carregando

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,16 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios";
+import Loader from "react-loader-spinner";
 import { Image, Form, StyledLink } from "./style";
 import logo from "../../images/logo.png";
 
 export default function LoginPage(){
+    const [ token, setToken ] = useState("");
+    const [ isEnabled, setIsEnabled ] = useState(true);
+    const [ loginData, setLoginData ] = useState({
+        email: "",
+        password: ""
+    });
+
+    function handleLogin(e){
+        e.preventDefault();
+        setIsEnabled(false)
+        const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", 
+        loginData);
+
+        promise.then(response => {
+            setIsEnabled(true)
+            console.log(response)
+            console.log(response.data.token)
+            setToken(response.data.token)
+        })
+        promise.catch(error => {
+            setIsEnabled(true)
+            console.log(error.response)
+        })
+    }
+
+    function handleInputChange(e){
+        setLoginData({...loginData, [e.target.name]: e.target.value})
+    }
+
     return(
         <>
         <Image src={logo} alt="logo TrackIt"/>
-        <Form>
-            <input type="email" placeholder="email" name="email" required />
-            <input type="password" placeholder="senha" name="password" required />
+        <Form onSubmit={handleLogin} enabled={isEnabled}>
+            <fieldset disabled={!isEnabled}>
+                <input type="email" placeholder="email" name="email" value={loginData.email} onChange={handleInputChange} required />
+                <input type="password" placeholder="senha" name="password" value={loginData.password} onChange={handleInputChange} required />
 
-            <button>Entrar</button>
+                <button type="submit">{isEnabled ? "Entrar" : <Loader type="ThreeDots" color="#FFF" height={13} width={38} />}</button>
+            </fieldset>
         </Form>
         <StyledLink to="/cadastro">Não tem uma conta? Cadastre-se!</StyledLink>
         </>
--- src/components/LoginPage/style.js
+++ src/components/LoginPage/style.js
@@ -7,33 +7,39 @@ export const Image = styled.img`
 `
 
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-input, button{
-    width: 303px;
-    height: 45px;
-
-    margin-bottom: 6px;
-    border: solid 1px #D4D4D4;
-    border-radius: 5px;
-    padding: 9px 11px;
-
-    ::placeholder{
-        font-size: 20px;
-        color: #DBDBDB;
+    fieldset{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
-}
 
-button{
-    background-color: #52B6FF;
-    font-size: 21px;
-    color: #FFFFFF;
 
-    margin-bottom: 25px;
-}
+    input, button{
+        background-color: ${(props) => props.enabled ? "#FFFFFF" : "#F2F2F2"};
+        color: ${(props) => props.enabled ? "#000" : "#AFAFAF"};
+        width: 303px;
+        height: 45px;
+
+        margin-bottom: 6px;
+        border: solid 1px #D4D4D4;
+        border-radius: 5px;
+        padding: 9px 11px;
+
+        ::placeholder{
+            font-size: 20px;
+            color: #DBDBDB;
+        }
+    }
+
+    button{
+        background-color: #52B6FF;
+        opacity: ${(props) => props.enabled ? 1 : 0.7};
+        font-size: 21px;
+        color: #FFFFFF;
+
+        margin-bottom: 25px;
+    }
 `;
 
 export const StyledLink = styled(Link)`
